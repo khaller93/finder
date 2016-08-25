d3a52009e1d04df0044797b842768fb25210832b
Kevin Haller <kevin.haller@outofbits.com>
2016-08-25 03:41:33 +0200

    #1: Changes the implementation of the VocabularyManager so that the spatial ontology builds the core ontology. All owl:imports are performed by considering the given specification of the document manager. It enables caching of the models and provides alternative paths for the case that a vocabulary to import cannot be fetched from its IRI. These alternate paths point to a representation stored on the local file system. The vocabulary manager now also methods to retrieve the raw model of local ontologies.

--- vocabulary/src/main/java/at/ac/tuwien/finder/vocabulary/VocabularyManager.java
+++ vocabulary/src/main/java/at/ac/tuwien/finder/vocabulary/VocabularyManager.java
@@ -3,9 +3,10 @@ package at.ac.tuwien.finder.vocabulary;
 import at.ac.tuwien.finder.vocabulary.exception.OntologyAccessException;
 import org.apache.jena.ontology.OntDocumentManager;
 import org.apache.jena.ontology.OntModel;
+import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.riot.Lang;
-import org.apache.jena.riot.RDFLanguages;
+import org.apache.jena.riot.RDFDataMgr;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -24,75 +25,95 @@ public final class VocabularyManager {
 
     private static final Logger logger = LoggerFactory.getLogger(VocabularyManager.class);
 
-    private static final String DC_ONT_MANAGER_CONF_PATH = "config/ont-manager.rdf";
+    private static final String ONTOLOGY_BASE = "http://finder.tuwien.ac.at/vocab/spatial#";
+    private static final String DC_ONT_MANAGER_CONF_PATH = "config/ontmanager-specification.rdf";
     private static final String SPATIAL_ONTOLOGY_PATH = "local/tuViennaSpatialOntology.ttl";
-    private static final String ORGANIZATION_ONTOLOGY_PATH = "local/tuWienOrganization.ttl";
 
-    /* ontology models */
-    private static OntModel spatialOntology;
-    private static OntModel organizationOntology;
+    private static VocabularyManager vocabularyManager;
+
+    /**
+     * Gets an instance of {@link VocabularyManager} that is designed to be a singleton class.
+     *
+     * @return an instance of {@link VocabularyManager} that is designed to be a singleton class.
+     * @throws OntologyAccessException if the core ontology cannot be accessed.
+     */
+    public static VocabularyManager getInstance() throws OntologyAccessException {
+        if (vocabularyManager == null) {
+            vocabularyManager = new VocabularyManager();
+        }
+        return vocabularyManager;
+    }
+
+    private OntModel coreOntology;
+    private Map<String, Model> ontologyMap;
+
+    /**
+     * Creates a new instance of {@link VocabularyManager}.
+     */
+    private VocabularyManager() throws OntologyAccessException {
+        coreOntology = initCoreOntology();
+        ontologyMap = initRawOntologyModels();
+    }
 
     /**
      * Gets the ontology that models the domain of spatial information about a university.
      *
      * @return the ontology that models the domain of spatial information about a university.
-     * @throws OntologyAccessException if the ontology can not be accessed.
      */
-    public static OntModel getSpatialOntology() throws OntologyAccessException {
-        if (spatialOntology == null) {
-            spatialOntology = readOntology(SPATIAL_ONTOLOGY_PATH, RDFLanguages.TURTLE);
-        }
-        return spatialOntology;
+    public OntModel getCoreOntology() {
+        return coreOntology;
     }
 
     /**
-     * Gets the ontology that models the domain of the organizational structure of an university.
+     * Gets a raw {@link Model} of the local spatial ontology.
      *
-     * @return the ontology that models the domain of the organizational structure of an university.
-     * @throws OntologyAccessException if the ontology can not be accessed.
+     * @return a raw {@link Model} of the local spatial ontology.
      */
-    public static OntModel getUniversityOrganizationOntology() throws OntologyAccessException {
-        if (organizationOntology == null) {
-            organizationOntology = readOntology(ORGANIZATION_ONTOLOGY_PATH, RDFLanguages.TURTLE);
-        }
-        return organizationOntology;
+    public Model getSpatialOntology() {
+        return ontologyMap.get("spatial");
     }
 
     /**
-     * Reads in the ontology of the given path.
+     * Reads in all local ontologies and stores them into an ontology map.
      *
-     * @param path     the path of the ontology file.
-     * @param language the language (rdf) of the ontology file.
-     * @return the ontology of the given path.
-     * @throws OntologyAccessException if the ontology can not be accessed.
+     * @return the map containing {@link Model} for each local ontology.
+     * @throws OntologyAccessException if a local ontology cannot be accessed.
      */
-    private static OntModel readOntology(String path, Lang language)
-        throws OntologyAccessException {
+    private Map<String, Model> initRawOntologyModels() throws OntologyAccessException {
+        Map<String, Model> ontologyMap = new HashMap<>();
+        Model spatialOntologyModel = ModelFactory.createDefaultModel();
+        try (InputStream spatialOntologyStream = VocabularyManager.class.getClassLoader()
+            .getResourceAsStream(SPATIAL_ONTOLOGY_PATH)) {
+            RDFDataMgr.read(spatialOntologyModel, spatialOntologyStream, Lang.TURTLE);
+            ontologyMap.put("spatial", spatialOntologyModel);
+        } catch (IOException e) {
+            logger.error("The spatial ontology located at '{}' cannot be read in. {}",
+                SPATIAL_ONTOLOGY_PATH, e);
+            throw new OntologyAccessException(e);
+        }
+        return ontologyMap;
+    }
+
+    /**
+     * Initializes the core ontology.
+     *
+     * @return {@link OntModel} that contains a spatial ontology.
+     * @throws OntologyAccessException if the core ontology cannot be initiated.
+     */
+    private OntModel initCoreOntology() throws OntologyAccessException {
         OntModel ontologyModel = ModelFactory.createOntologyModel();
         OntDocumentManager documentManager = ontologyModel.getDocumentManager();
         documentManager.setMetadataSearchPath(DC_ONT_MANAGER_CONF_PATH, true);
-        try (InputStream facilityOntStream = VocabularyManager.class.getClassLoader()
-            .getResourceAsStream(path)) {
-            ontologyModel.read(facilityOntStream, null, language.getName());
+        try (InputStream spatialOntologyStream = VocabularyManager.class.getClassLoader()
+            .getResourceAsStream(SPATIAL_ONTOLOGY_PATH)) {
+            ontologyModel.read(spatialOntologyStream, ONTOLOGY_BASE, Lang.TURTLE.getName());
         } catch (IOException e) {
-            logger.error("The ontology {} can not be read in. {}", path, e);
+            logger.error("The spatial ontology located at '{}' cannot be read in. {}",
+                SPATIAL_ONTOLOGY_PATH, e);
             throw new OntologyAccessException(e);
         }
-        logger.debug(ontologyModel.toString());
+        logger.debug("Initialized core ontology {}.", ontologyModel.toString());
         return ontologyModel;
-    }
-
-    /**
-     * Gets all core ontologies. The key of the map is the namespace of the ontology.
-     *
-     * @return all core ontologies.
-     * @throws OntologyAccessException if the ontology can not be accessed.
-     */
-    public static Map<String, OntModel> getCoreOntologies() throws OntologyAccessException {
-        Map<String, OntModel> coreOntologies = new HashMap<>();
-        coreOntologies.put(TuViennaSpatialOntology.getURI(), getSpatialOntology());
-        coreOntologies.put(TuWienOrganization.getURI(), getUniversityOrganizationOntology());
-        return coreOntologies;
     }
 
 }

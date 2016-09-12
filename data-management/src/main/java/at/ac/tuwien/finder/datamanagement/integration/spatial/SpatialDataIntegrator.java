a9af8ff43493a5c68b2c1ab5f3e814aa40d7e9b0
Kevin Haller <kevin.haller@outofbits.com>
2016-09-12 23:16:07 +0200

    #2: After each integration of new linked data into the spatial dataset a update of the metadata regarding this dataset will be requested. Date of last modification will be updated.

--- data-management/src/main/java/at/ac/tuwien/finder/datamanagement/integration/spatial/SpatialDataIntegrator.java
+++ data-management/src/main/java/at/ac/tuwien/finder/datamanagement/integration/spatial/SpatialDataIntegrator.java
@@ -1,7 +1,9 @@
 package at.ac.tuwien.finder.datamanagement.integration.spatial;
 
 import at.ac.tuwien.finder.datamanagement.TripleStoreManager;
+import at.ac.tuwien.finder.datamanagement.catalog.dataset.SimpleSpatialDataSet;
 import at.ac.tuwien.finder.datamanagement.catalog.dataset.SpatialDataSet;
+import at.ac.tuwien.finder.datamanagement.catalog.exception.DataCatalogException;
 import at.ac.tuwien.finder.datamanagement.integration.DataIntegrator;
 import at.ac.tuwien.finder.datamanagement.integration.exception.TripleStoreManagerException;
 import org.eclipse.rdf4j.model.Model;
@@ -9,6 +11,8 @@ import org.eclipse.rdf4j.repository.RepositoryConnection;
 import org.eclipse.rdf4j.repository.RepositoryException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Date;
 
 /**
  * This is an implementation of {@link DataIntegrator} that integrates linked spatial data about
@@ -32,7 +36,7 @@ public class SpatialDataIntegrator implements DataIntegrator {
 
     @Override
     public String graphName() {
-        return SpatialDataSet.NS.stringValue();
+        return SimpleSpatialDataSet.NS.stringValue();
     }
 
     @Override
@@ -45,6 +49,13 @@ public class SpatialDataIntegrator implements DataIntegrator {
         try {
             connection = tripleStoreManager.getConnection();
             connection.add(model, SpatialDataSet.NS);
+            try {
+                tripleStoreManager.getDataCatalog().get(SpatialDataSet.NS).modifiedAt(new Date());
+            } catch (DataCatalogException e) {
+                logger
+                    .error("Modification update for the dataset ({}) failed. {}", SpatialDataSet.NS,
+                        e);
+            }
         } catch (RepositoryException e) {
             e.printStackTrace();
         } finally {

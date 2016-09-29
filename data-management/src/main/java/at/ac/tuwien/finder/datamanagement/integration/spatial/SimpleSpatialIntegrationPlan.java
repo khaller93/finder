e0dc43b08d5a33b29ea72179fc5413f43d980ccb
Kevin Haller <kevin.haller@outofbits.com>
2016-09-30 01:24:31 +0200

    #2: New constructors for the integration plan to enable a better configuration of linker.

--- data-management/src/main/java/at/ac/tuwien/finder/datamanagement/integration/spatial/SimpleSpatialIntegrationPlan.java
+++ data-management/src/main/java/at/ac/tuwien/finder/datamanagement/integration/spatial/SimpleSpatialIntegrationPlan.java
@@ -1,11 +1,10 @@
 package at.ac.tuwien.finder.datamanagement.integration.spatial;
 
 import at.ac.tuwien.finder.datamanagement.catalog.dataset.SpatialDataSet;
-import at.ac.tuwien.finder.datamanagement.integration.DataCleanser;
-import at.ac.tuwien.finder.datamanagement.integration.DataLinker;
-import at.ac.tuwien.finder.datamanagement.integration.IntegrationPlan;
-import at.ac.tuwien.finder.datamanagement.integration.SimpleDataIntegrator;
+import at.ac.tuwien.finder.datamanagement.integration.*;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 
 /**
@@ -18,9 +17,55 @@ public class SimpleSpatialIntegrationPlan extends IntegrationPlan {
 
     /**
      * Creates a new simple facility integration plan. The plan includes a
-     * {@link SimpleDataIntegrator}, no {@link DataLinker} and no {@link DataCleanser}.
+     * {@link SimpleDataIntegrator}, no {@link DataLinker} and no {@link DataCleanser}. For the
+     * {@link SimpleDataIntegrator} is no namespace mapping given.
      */
     public SimpleSpatialIntegrationPlan() {
         super(new SimpleDataIntegrator(SpatialDataSet.NS), Collections.emptyList(), null);
     }
+
+    /**
+     * Creates a new simple facility integration plan. The plan includes a
+     * {@link SimpleDataIntegrator}, the given {@link DataLinker}s and no {@link DataCleanser}.
+     *
+     * @param dataLinkers {@link DataLinker} that shall be used.
+     */
+    public SimpleSpatialIntegrationPlan(Collection<DataLinker> dataLinkers) {
+        super(new SimpleDataIntegrator(SpatialDataSet.NS), dataLinkers, null);
+    }
+
+    /**
+     * Creates a new simple facility integration plan. The plan includes the given
+     * {@link SimpleDataIntegrator} and {@link DataLinker}, but no {@link DataCleanser}.For the
+     * {@link SimpleDataIntegrator} is no namespace mapping given.
+     *
+     * @param dataIntegrator {@link DataIntegrator} that shall be used.
+     * @param dataLinkers    {@link DataLinker} that shall be used.
+     */
+    public SimpleSpatialIntegrationPlan(DataIntegrator dataIntegrator,
+        Collection<DataLinker> dataLinkers) {
+        super(dataIntegrator, dataLinkers, null);
+    }
+
+    /**
+     * Creates a new simple facility integration plan. The plan includes a
+     * {@link SimpleDataIntegrator}, given {@link DataLinker}s and no {@link DataCleanser}.
+     *
+     * @param dataLinkers {@link DataLinker} that shall be used.
+     */
+    public SimpleSpatialIntegrationPlan(DataLinker... dataLinkers) {
+        super(new SimpleDataIntegrator(SpatialDataSet.NS), Arrays.asList(dataLinkers), null);
+    }
+
+    /**
+     * Creates a new simple facility integration plan. The plan includes a
+     * {@link SimpleDataIntegrator}, given {@link DataLinker}s and no {@link DataCleanser}.
+     *
+     * @param dataIntegrator {@link DataIntegrator} that shall be used.
+     * @param dataLinkers    {@link DataLinker} that shall be used.
+     */
+    public SimpleSpatialIntegrationPlan(DataIntegrator dataIntegrator, DataLinker... dataLinkers) {
+        super(dataIntegrator, Arrays.asList(dataLinkers), null);
+    }
+
 }

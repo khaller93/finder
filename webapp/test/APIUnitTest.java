68ee1683ac1744562934da98ef32881645a9aa68
Kevin Haller <kevin.haller@outofbits.com>
2016-10-07 11:31:20 +0200

    #8: Changes the web application unit test so that provision is possible, if the database is not running.

--- webapp/test/APIUnitTest.java
+++ webapp/test/APIUnitTest.java
@@ -37,7 +37,7 @@ import static org.mockito.Mockito.when;
  * Simple (JUnit) tests that can call all parts of a play app.
  * If you are interested in mocking a whole application, see the wiki for more details.
  */
-public class APIUnitTest extends WithApplication {
+public class APIUnitTest {
 
     private static TripleStoreManager tripleStoreManager;
 
